f7d8ffe72fadefed4caf99de5eefcdc13acd6286
Wyatt Jackson <wyattcjackson2006@outlook.com>
2021-03-24 07:51:05 -0500

    Copy day  23's work to day 24.

---
+++ 2021/march/24/spaceshipInterface/spaceshipInterface.ino
@@ -0,0 +1,47 @@
+int switchState = 0;
+
+void setup() {
+	// Set LED pins to OUTPUT
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(5, OUTPUT);
+
+	// Set button pin to INPUT
+	pinMode(2, INPUT);
+}
+
+void loop() {
+	// Sets digitalRead to the state of the button.
+	switchState = digitalRead(2);
+
+	if (switchState == LOW)
+	{
+		// The button is not pressed
+		digitalWrite(3, HIGH);	// Turn on green led
+		digitalWrite(4, LOW);	// Turn off red led 1
+		digitalWrite(5, LOW);	// Tun off red led 2
+	}
+	else
+	{
+		// The button is pressed
+		digitalWrite(3, LOW);	// Turn off green led
+		digitalWrite(4, HIGH);	// Turn on red led 1
+		digitalWrite(5, HIGH);	// Turn on red led 2
+
+		delay(250);
+		flashRedLeds();
+	}
+}
+
+void flashRedLeds()
+{
+	while (switchState == HIGH)
+	{
+		digitalWrite(4, LOW);
+		digitalWrite(5, HIGH);
+		delay(250);				// Wail for 0.25 seconds
+		digitalWrite(4, HIGH);
+		digitalWrite(5, LOW);
+	}
+	loop();
+}
