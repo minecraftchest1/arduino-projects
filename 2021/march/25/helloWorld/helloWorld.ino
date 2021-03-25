f828c6b5c0d1e7e32cb6c32870e6de8698acbba2
Minecraftchest1 <minecraftchest1@outlook.com>
2021-03-25 14:33:19 -0500

    Add missing double quote.

--- 2021/march/25/helloWorld/helloWorld.ino
+++ 2021/march/25/helloWorld/helloWorld.ino
@@ -13,7 +13,7 @@ LiquidCrystal LCD(12,11,5,4,3,2);						// Initilize the LCD object
 #define PG4LN2 "@outlook.com"
 
 #define PG5LN1 "Minecraftchest1"
-#define PG5LN2 ".wordpress.com
+#define PG5LN2 ".wordpress.com"
 
 #define WAIT 2500
 
